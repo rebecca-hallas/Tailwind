8417dd7b93aaa38fbb976b791f174904038bdfe3
Rebecca-Hallas <rebeccajhallas@gmail.com>
2021-05-27 10:59:13 +1200

    edit tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,12 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: theme => ({
-        'code': "url('./bg.png')",
-        'yay': "url('./yay.png')",
-        'woo': "url('./woo.png')",
-        'yeya': "url('./yeya.png')",
-        'c1r1': "url('./images/c1r1.png')",
         'demo1': "url('./images/demo1.png')",
         'demo2': "url('./images/demo2.png')",
         'demo3': "url('./images/demo3.png')",
         'demo4': "url('./images/demo4.png')",
-        'demo5': "url('./images/demo5.png')"
+        'demo5': "url('./images/demo5.png')",
+        'grid': "url('./images/demo5.png')"
       }),
       fontFamily: {
         'poppins': ['poppins', 'Arial']
