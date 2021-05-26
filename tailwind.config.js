1571e421c2691eea607a8a675358625fbfdb75bf
Rebecca-Hallas <rebeccajhallas@gmail.com>
2021-05-26 21:46:49 +1200

    all grid images in place

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,15 @@ module.exports = {
         'code': "url('./bg.png')",
         'yay': "url('./yay.png')",
         'woo': "url('./woo.png')",
-        'yeya': "url('./yeya.png')"
+        'yeya': "url('./yeya.png')",
+        'c1r1': "url('./images/c1r1.png')",
+        'demo1': "url('./images/demo1.png')",
+        'demo2': "url('./images/demo2.png')",
+        'demo3': "url('./images/demo3.png')",
+        'demo4': "url('./images/demo4.png')",
+        'demo5': "url('./images/demo5.png')"
+
+
       }),
     },
   },
